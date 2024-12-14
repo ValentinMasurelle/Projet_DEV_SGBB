f7712670187669614b6d758948a6e60217414b5b
Masurelle Valentin <valentin.masurelle@hainaut-promsoc.be>
2024-12-14 17:03:43 +0100

    add the documentation on init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,31 @@
-GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+/*=============================================================
+|                   🏃 QUERY SportifCourse                    | 
+|-------------------------------------------------------------|
+|  📌 AUTHOR      : Masurelle Valentin                        |
+|  📅 DATE        : 2024-12-14                                |
+|  📝 DESCRIPTION : GRANT PRIVILEGES TO USER TEST             |	
+	                                                          |
+|                                                             |
+|                                                             |
+|                                                             |
+|  🗄️ DATABASE    : ALL DATABASES                           |
+=============================================================*/
 
+
+
+-- ------------------------------------------------------------
+-- GRANT PRIVILEGES TO USER
+-- ------------------------------------------------------------
+
+-- Grant all privileges on all databases and tables (*) to the user 'test' 
+-- from any host ('%'). The user also gets the 'WITH GRANT OPTION' 
+-- which allows them to grant privileges to others.
+GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' WITH GRANT OPTION;
+
+-- ------------------------------------------------------------
+-- FLUSH PRIVILEGES
+-- ------------------------------------------------------------
+
+-- Apply the privilege changes by reloading the grant tables
+-- This ensures that the changes made by the GRANT command take effect immediately.
+FLUSH PRIVILEGES;
