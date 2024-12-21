df9cef0f5a80ef1a16a2a4d9e4c6f10604be2087
Masurelle Valentin <valentin.masurelle@hainaut-promsoc.be>
2024-12-21 19:01:26 +0100

    add the documentation

--- web_php/BiblioFinal/models/LivreManager.class.php
+++ web_php/BiblioFinal/models/LivreManager.class.php
@@ -1,27 +1,88 @@
 <?php
-require_once "Model.class.php";
-require_once "Livre.class.php";
+/*=============================================================
+|                   📝 CLASS: LivreManager                     |
+|-------------------------------------------------------------|
+|  👨‍💻 AUTHOR      : Masurelle Valentin                        |
+|  📅 DATE        : 2024-12-21                                |
+|  📝 DESCRIPTION : This class manages a collection of books 
+|                  (`Livre` objects). It provides methods 
+|                  to add books to the collection, retrieve 
+|                  the list of books, and load books from the 
+|                  database into the collection.              |
+=============================================================*/
 
-class LivreManager extends Model{
+/**
+ * LivreManager Class
+ * 
+ * This class handles the management of a collection of `Livre` objects.
+ * It provides functionality to add books, retrieve them, and populate
+ * the collection from the database using a view (`v_livres`).
+ */
+class LivreManager extends Model {
+    /**
+     * @var Livre[] $livres Collection of Livre objects.
+     */
     private $livres;
 
-    public function ajoutLivre($livre){
-        $this->livres[] = $livre;
+    /**
+     * Adds a `Livre` object to the collection.
+     *
+     * This method appends the provided `Livre` object to the internal array
+     * of books (`$livres`).
+     *
+     * @param Livre $livre The book object to be added.
+     * @return void
+     */
+    public function ajoutLivre($livre) {
+        $this->livres[] = $livre;  // Append the Livre object to the livres array.
     }
 
-    public function getLivres(){
-        return $this->livres;
+    /**
+     * Retrieves the collection of books (`Livre` objects).
+     *
+     * This method returns the current list of `Livre` objects that are 
+     * stored in the `$livres` property.
+     *
+     * @return Livre[] Array of Livre objects.
+     */
+    public function getLivres() {
+        return $this->livres;  // Return the array of Livre objects.
     }
 
-    public function chargementLivres(){
+    /**
+     * Loads books from the database and populates the collection.
+     * 
+     * This method executes a SQL query to fetch book data from the view 
+     * `v_livres`. It creates `Livre` objects for each row and adds them 
+     * to the `$livres` collection.
+     *
+     * @return void
+     * @throws Exception If database access fails.
+     */
+    public function chargementLivres() {
+        // Prepare and execute the SQL query to fetch book data
         $req = $this->getBdd()->prepare("SELECT * FROM v_livres");
         $req->execute();
+
+        // Fetch all books as an associative array
         $mesLivres = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        // Close the cursor to free up the database resources
         $req->closeCursor();
 
-        foreach($mesLivres as $livre){
-            $l = new Livre($livre['id'],$livre['titre'],$livre['nbPages'],$livre['image'],$livre['editeur']);
-            $this->ajoutLivre($l);
+        // Loop through each row of the fetched book data
+        foreach ($mesLivres as $livre) {
+            // Create a new Livre object using the fetched data
+            $l = new Livre(
+                $livre['id'],    // Book ID
+                $livre['titre'], // Book title
+                $livre['nbPages'], // Number of pages
+                $livre['image'],  // Book image
+                $livre['editeur'] // Book editor (from the database column 'editeur')
+            );
+            // Add the created Livre object to the collection
+            $this->ajoutLivre($l);  // Add the Livre object to the collection
         }
     }
 }
+?>
